1a62096f3d06ff514f6360b721ee9224446ec3c6
Brandon Konkle <brandon@konkle.us>
2013-06-19 12:10:35 -0500

    Fix grunt inside runserveR

--- checkoff/core/management/commands/runserver.py
+++ checkoff/core/management/commands/runserver.py
@@ -5,17 +5,24 @@ import signal
 from optparse import make_option
 
 from django.conf import settings
-from django.core.management.commands.runserver import Command as RunserverCommand
+from django.contrib.staticfiles.management.commands.runserver import Command\
+    as StaticfilesRunserverCommand
 
 
-class Command(RunserverCommand):
-    option_list = RunserverCommand.option_list + (
-        make_option('--grunt', action='store_false', dest='grunt', help='Runs grunt watch at the same time as runserver'),
+class Command(StaticfilesRunserverCommand):
+    option_list = StaticfilesRunserverCommand.option_list + (
+        make_option(
+            '--no-grunt',
+            default=True,
+            action='store_false',
+            dest='grunt',
+            help='Prevents grunt from running at the same time as runserver'
+        ),
     )
 
     def inner_run(self, *args, **options):
         grunt = options.get('grunt')
-        if grunt is not None:
+        if grunt:
             self.start_grunt()
         return super(Command, self).inner_run(*args, **options)
 
